2b0b7951107f780c5a7b21b55232a4d3ea6b56d6
koshish khadka <koshishkhadka364@gmail.com>
2025-11-21 21:28:10 +0545

    fetched all disaster

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,6 +5,21 @@ import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import L from "leaflet";
 import { useEffect, useState } from "react";
+import { getAllDisasters } from "@/actions/disaster";
+
+type Disaster = {
+  id: string;
+  created_by: string;
+  description: string;
+  enddate: string;
+  lat: number;
+  lng: number;
+  startdate: string;
+  title: string;
+  status: string;
+  location: string;
+  radius: number;
+};
 
 export default function Map({ resources }: { resources: resourceDataType[] }) {
   // console.log("Map component", resources);
@@ -12,6 +27,8 @@ export default function Map({ resources }: { resources: resourceDataType[] }) {
   const [location, setLocation] = useState<{ lat: number; lng: number } | null>(
     null
   );
+
+  const [disasters, setDisasters] = useState<Disaster[]>([]);
 
   useEffect(() => {
     if ("geolocation" in navigator) {
@@ -31,8 +48,28 @@ export default function Map({ resources }: { resources: resourceDataType[] }) {
     }
   }, []);
 
+  // Fetch all disasters on component mount
+  useEffect(() => {
+    const fetchDisasters = async () => {
+      const response = await getAllDisasters();
+      if (response.status === "success") {
+        // console.log(response.data);
+        setDisasters(response.data || []);
+      } else {
+        console.log("Failed to fetch disaster", response.message);
+      }
+    };
+    fetchDisasters();
+  }, []);
+
   const flagIcon = L.icon({
     iconUrl: "/images/flag2.png", // Public folder path
+    iconSize: [32, 32], // Adjust size
+    iconAnchor: [16, 32], // Anchor at bottom-center
+    popupAnchor: [0, -32], // Popup above icon
+  });
+  const flagIcon2 = L.icon({
+    iconUrl: "/images/flag.png", // Public folder path
     iconSize: [32, 32], // Adjust size
     iconAnchor: [16, 32], // Anchor at bottom-center
     popupAnchor: [0, -32], // Popup above icon
@@ -69,6 +106,11 @@ export default function Map({ resources }: { resources: resourceDataType[] }) {
 
       {resources.map((item, index) => (
         <Marker key={index} position={[item.lat, item.lng]} icon={flagIcon}>
+          <Popup>{item.title}</Popup>
+        </Marker>
+      ))}
+      {disasters.map((item, index) => (
+        <Marker key={index} position={[item.lat, item.lng]} icon={flagIcon2}>
           <Popup>{item.title}</Popup>
         </Marker>
       ))}
